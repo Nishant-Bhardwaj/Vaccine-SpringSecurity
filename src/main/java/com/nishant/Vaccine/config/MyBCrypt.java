24c8b9b0de3e8c0853e7b0cace4ba954a87c8f08
NISHANT  BHARDWAJ <bhardwaj.nishant1995@gmail.com>
2021-06-24 21:30:16 +0530

    Adding InMemoryUserDetails for into Login app

---
+++ src/main/java/com/nishant/Vaccine/config/MyBCrypt.java
@@ -0,0 +1,15 @@
+package com.nishant.Vaccine.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class MyBCrypt {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+}
--- src/main/java/com/nishant/Vaccine/config/MySecurityConfig.java
+++ src/main/java/com/nishant/Vaccine/config/MySecurityConfig.java
@@ -1,15 +1,30 @@
 package com.nishant.Vaccine.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public MySecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
 //        http.csrf().disable();
 
         http
@@ -20,6 +35,22 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin();
         
 //        http.formLogin().disable();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+
+        UserDetails nishantUser = User.builder()
+                .username("nishant")
+                .password(passwordEncoder.encode("Talent@2020"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                nishantUser
+        );
 
     }
+
 }
